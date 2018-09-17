5cf4c953ccb77f699aee8e8c8e5a1cd9c766a9a9
Tamas Elekes <elekestamas@protonmail.com>
2018-09-17 16:20:07 +0200

    updated script file

--- build_run.sh
+++ build_run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+export LD_LIBRARY_PATH=./
 javac -cp contest.jar *.java
 jar cmf MainClass.txt submission.jar *.class
 java -jar testrun.jar -submission=player2 -evaluation=BentCigarFunction -seed=1
