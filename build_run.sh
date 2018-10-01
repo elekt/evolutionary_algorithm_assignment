eaaf4cb9336cbfba55a9c62d0025a81cf9d031cf
Tamas Elekes <elekestamas@protonmail.com>
2018-10-01 12:32:58 +0200

    enabled cleaning at the end of build and run

--- build_run.sh
+++ build_run.sh
@@ -4,4 +4,4 @@ javac -cp contest.jar *.java
 jar cmf MainClass.txt submission.jar *.class
 jar uf testrun.jar *.class
 java -jar testrun.jar -submission=player2 -evaluation=BentCigarFunction -seed=1
-#python clean.py
+python clean.py
