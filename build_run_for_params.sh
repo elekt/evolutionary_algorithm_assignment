84e1dbc9d54097437a606da1e8d76bc6c876e52a
Tamas Elekes <elekestamas@protonmail.com>
2018-10-05 15:03:04 +0200

    parameter setting script WIP

---
+++ build_run_for_params.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+javac -cp contest.jar *.java
+jar cmf MainClass.txt submission.jar *.class
+java -Dvar1=$1 -jar testrun.jar -submission=player2 -evaluation=SchaffersEvaluation -seed=1
--- parameter_tuning.py
+++ parameter_tuning.py
@@ -1,8 +1,20 @@
 import subprocess
 
-params = "InversionMutationProbability:0.6,SimpleMutationProbability:0.2,SimpleMutationSpeed:1.5,SwapMutationProbability:0.6,SwapMutationNumberOfSwaps:3,ScrambleMutationProbability:0.6,RankingSelectionSUSMatingPoolSize:2,RankingSelectionSUSs:1.3,TournamentSelectionMatingPoolSize:2,TournamentSelectionNumberOfParticipiants:4,UniformParentSelectionMatingPoolSize:2,ParentSelectionMethod:1,CrossoverMethod:2"
+# "InversionMutationProbability: 0.0 - 1.0
+# "SimpleMutationProbability: 0.0 - 1.0
+# "SimpleMutationSpeed: 0.0 - 2.5
+# "SwapMutationProbability: 0.0 - 1.0
+# "SwapMutationNumberOfSwaps: 0 - 5
+# "ScrambleMutationProbability: 0.0 - 1.0
+# "RankingSelectionSUSMatingPoolSize: 0 - 6 % 2
+# "RankingSelectionSUSs: 1.0001 - 2.0
+# "TournamentSelectionMatingPoolSize: 2 - 8 % 2
+# "TournamentSelectionNumberOfParticipiants: 1 - 8
+# "UniformParentSelectionMatingPoolSize: 2 - 8 % 2
 
 
-result = subprocess.check_output(['/home/redmachine/Desktop/Uva-DataScience/EvolutionaryComputing/assignmentfiles_2017/build_run.sh', params])
+params = "InversionMutationProbability:{},SimpleMutationProbability:{},SimpleMutationSpeed:{},SwapMutationProbability:{},SwapMutationNumberOfSwaps:{},ScrambleMutationProbability:{},RankingSelectionSUSMatingPoolSize:{},RankingSelectionSUSs:{},TournamentSelectionMatingPoolSize:{},TournamentSelectionNumberOfParticipiants:{},UniformParentSelectionMatingPoolSize:{},ParentSelectionMethod:{},CrossoverMethod:{}"
+
+result = subprocess.check_output(['/home/redmachine/Desktop/Uva-DataScience/EvolutionaryComputing/assignmentfiles_2017/build_run_for_params.sh', params])
 
 print(result)
