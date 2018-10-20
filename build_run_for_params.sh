25b0866259842cddedf451bf4d4903428a669b47
jeromemies <jeromemies@hotmail.com>
2018-10-20 22:01:16 +0200

    Update build_run_for_params.sh

--- build_run_for_params.sh
+++ build_run_for_params.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH=./
 javac -cp contest.jar *.java
 jar cmf MainClass.txt submission.jar *.class
-java -Xss1g -Dvar1=$1 -jar testrun.jar -submission=player2 -evaluation=$2 -seed=1
+java -Dvar1=$1 -jar testrun.jar -submission=player2 -evaluation=$2 -seed=1
