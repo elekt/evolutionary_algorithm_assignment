2ea7a3a956517fc11763caf5820a56df3dfba2ed
Tamas Elekes <elekestamas@protonmail.com>
2018-10-21 12:53:42 +0200

    data processing

--- parameter_schaffers.py
+++ parameter_schaffers.py
@@ -8,43 +8,46 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 
-with open('bent_cigar_crowding.pkl', 'rb') as input:
-    result_bent_cigar_array = pickle.load(input)
-    bent_cigar_scores = [param["score"] for param in result_bent_cigar_array]
-    bent_cigar_scores  = np.array(bent_cigar_scores).astype(np.float)
+with open('schaffers_benchmark.pkl', 'rb') as input:
+    result_schaffers_benchmark_array = pickle.load(input)
+    schaffers_benchmark_scores = [param["intermediate_scores"] for param in result_schaffers_benchmark_array]
+    schaffers_benchmark_scores  = np.array(schaffers_benchmark_scores[3]).astype(np.float)
 
 with open('schaffers_crowding.pkl', 'rb') as input:
-    result_schaffers_array = pickle.load(input)
-    schaffers_scores = [param["score"] for param in result_schaffers_array]
-    schaffers_scores  = np.array(schaffers_scores).astype(np.float)
+    result_schaffers_crowding_array = pickle.load(input)
+    schaffers_crowding_scores = [param["intermediate_scores"] for param in result_schaffers_crowding_array]
+    schaffers_crowding_scores  = np.array(schaffers_crowding_scores[3]).astype(np.float)
 
-with open('katsuura_crowding.pkl', 'rb') as input:
-    result_katsuura_array = pickle.load(input)
-    katsuura_scores = [param["score"] for param in result_katsuura_array]
-    katsuura_scores  = np.array(katsuura_scores).astype(np.float)
+with open('schaffers_islands_400.pkl', 'rb') as input:
+    result_schaffers_islands_array = pickle.load(input)
+    schaffers_islands_scores = [param["intermediate_scores"] for param in result_schaffers_islands_array]
+    schaffers_islands_scores  = np.array(schaffers_islands_scores[3]).astype(np.float)
 
 
-print(np.mean(bent_cigar_scores))
-print(np.mean(schaffers_scores))
-print(np.mean(katsuura_scores))
+print(np.mean(schaffers_benchmark_scores))
+print(np.mean(schaffers_crowding_scores))
+print(np.mean(schaffers_islands_scores))
 
-fig = plt.figure()
-fig.suptitle('Crodwing algorithm', fontsize=14, fontweight='bold')
+# fig = plt.figure()
+# fig.suptitle('Crodwing algorithm', fontsize=14, fontweight='bold')
+#
+# ax = fig.add_subplot(131)
+# ax.boxplot(bent_cigar_scores)
+# ax.set_title('Bent Cigar')
+# ax.set_ylabel('Score')
+# ax.set_yticks(range(5, 11))
+#
+# ax = fig.add_subplot(132)
+# ax.boxplot(schaffers_scores)
+# ax.set_title('Schaffers')
+# ax.set_yticks(range(5, 11))
+#
+# ax = fig.add_subplot(133)
+# ax.boxplot(katsuura_scores)
+# ax.set_title('Katsuura')
+# ax.set_yticks(range(5, 11))
 
-ax = fig.add_subplot(131)
-ax.boxplot(bent_cigar_scores)
-ax.set_title('Bent Cigar')
-ax.set_ylabel('Score')
-ax.set_yticks(range(5, 11))
-
-ax = fig.add_subplot(132)
-ax.boxplot(schaffers_scores)
-ax.set_title('Schaffers')
-ax.set_yticks(range(5, 11))
-
-ax = fig.add_subplot(133)
-ax.boxplot(katsuura_scores)
-ax.set_title('Katsuura')
-ax.set_yticks(range(5, 11))
-
+plt.plot(schaffers_benchmark_scores, color='orange')
+plt.plot(schaffers_crowding_scores, color='blue')
+plt.plot(schaffers_islands_scores, color='green')
 plt.show()
